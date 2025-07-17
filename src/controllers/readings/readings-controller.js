5af25ae6abf611cee3cedd6970a6e9b1a3962923
ishan.gupta <ishan.gupta@intimetec.com>
2025-07-17 22:05:43 +0530

    Implemented error handling and response handler

--- src/controllers/readings/readings-controller.js
+++ src/controllers/readings/readings-controller.js
@@ -1,22 +1,31 @@
 const { readingsData } = require("./readings.data");
 const { readings } = require("./readings");
 const { getReadings, setReadings } = readings(readingsData);
+const responseHandler = require("../../utils/responseHandler");
 
 const read = (req, res) => { 
-    const meter = req.params;
-    const response = getReadingsFromMeterId(meter)
-    res.send(response); // do error handling
+    try {
+        const meter = req.params;
+        const response = getReadingsFromMeterId(meter)
+        responseHandler.send(null,req,res,response);
+    } catch (error) {
+        responseHandler.send(error, req, res, null);
+    }
+    
 };
 
 const store = (req, res) => {
-    const data = req.body;
-    setMeterReadings(data)
-    res.send() // do error handling
+    try {
+        const data = req.body;
+        setMeterReadings(data)
+        responseHandler.send(null, req, res, null); 
+    } catch (error) {
+        responseHandler.send(error, req, res, null);
+    }
 }
 
 const getReadingsFromMeterId = (meter) => {
     const meterId = meter.smartMeterId;
-
     return getReadings(meterId);
 }
 
